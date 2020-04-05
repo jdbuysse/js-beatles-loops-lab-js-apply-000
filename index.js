75a45893e89fbe65ffba5b8179fb61a7f19a20ee
Jordan Buysse <jdbuysse@gmail.com>
2020-04-05 10:47:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 function theBeatlesPlay(musicians, instruments) {
   var result = [];
   for (let i = 0; i < musicians.length(); i++) {
-    
+    result.push(musicians[i] + " plays " + instruments[i]);
   }
-  
+}
+
+function johnLennonFacts(facts) {
+  while 
 }
