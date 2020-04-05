0ebd057bec5e6a7aed392178cb470f46b1e18b97
Jordan Buysse <jdbuysse@gmail.com>
2020-04-05 10:55:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   var result = [];
-  for (let i = 0; i < musicians.length(); i++) {
+  for (let i = 0; i < musicians.length; i++) {
     result.push(musicians[i] + " plays " + instruments[i]);
   }
 }
 
 function johnLennonFacts(facts) {
-  let n = facts.length(); //if let doesn't work with scope just try var
+  let n = facts.length; //if let doesn't work with scope just try var
   while (n < 1) {
     facts[n] = facts[n] + "!!!";
   }
